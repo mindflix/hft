4a4587a59e04a05e4ced0ebda3d58649efe675c8
mindflix <nyco.demol@gmail.com>
2021-10-20 09:35:26 +0200

    news

--- main.py
+++ main.py
@@ -1,44 +1,33 @@
+from binance import ThreadedWebsocketManager
 import os
-import pprint
-import json
+import time
 from dotenv import load_dotenv
-import websocket
 
 
 load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
 SECRET_KEY = os.getenv('SECRET_KEY')
-SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kstick_1m"
-
-closes = []
-
-
-def on_error(ws, error):
-    print(error)
-
-
-def on_close(ws):
-    print("### closed ###")
-
-
-def on_open(ws):
-    print("### opened ###")
-
-
-def on_message(ws, message):
-    json_message = json.loads(message)
-    pprint.pprint(json_message)
-
-    candle = json_message['k']
 
 
 def main():
-    ws = websocket.WebSocketApp(SOCKET, on_open=on_open,
-                                on_message=on_message,
-                                on_error=on_error,
-                                on_close=on_close)
-    ws.run_forever()
+
+    symbol = 'BTCUSDT'
+
+    twm = ThreadedWebsocketManager(api_key=API_KEY, api_secret=SECRET_KEY)
+    # start is required to initialise its internal loop
+    twm.start()
+
+    def handle_socket_message(msg):
+        symbol = msg["s"]
+        price = msg["p"]
+        quantity = msg["q"]
+        print(("SYMBOL: {} PRICE: {} QUANTITY: {}").format(
+            symbol, price, quantity))
+
+    twm.start_trade_socket(callback=handle_socket_message, symbol=symbol)
+
+    twm.join()
 
 
 if __name__ == "__main__":
